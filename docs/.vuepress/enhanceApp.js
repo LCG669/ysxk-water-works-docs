ef994463fcb3a6d543a78b2fae55edc64318b282
LCG669 <1191852902@qq.com>
2024-06-25 10:07:55 +0800

    维护滚动条

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -5,14 +5,40 @@ import '../../css/public.css'
 import ysxk_water_works from 'ysxk-water-works'
 import 'ysxk-water-works/lib/ysxk-water-works.css'
 
+// 导入滚动条 overlayscrollbars
+import 'overlayscrollbars/styles/overlayscrollbars.css'
+import {
+    OverlayScrollbars
+} from 'overlayscrollbars'
+
 // Vue.use(ysxk_water_works)
 
-export default async ({
+export default ({
     Vue
 }) => {
     if (typeof process === 'undefined') {
         Vue.use(ysxk_water_works)
     }
-}
 
-console.log('运行enhanceApp.js')
+    Vue.mixin({
+        mounted() {
+            // 找到所有 .demo-content 元素
+            
+            const demoContentElements = document.querySelectorAll('.demo-content')
+
+            demoContentElements.forEach(el => {
+                // 检查元素是否已经初始化过 OverlayScrollbars  
+                if (!el.dataset.isScrollbar) {
+                    // 初始化 OverlayScrollbars  
+                    OverlayScrollbars(el, { /* 配置选项 */ })
+
+                    // 添加自定义属性以标记已初始化
+                    el.dataset.isScrollbar = true
+                }
+            })
+        },
+    })
+
+    // 对 document.body 应用 OverlayScrollbars  
+    OverlayScrollbars(document.body, { /* 配置选项 */ })
+}
